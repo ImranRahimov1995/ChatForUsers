2fbcaa43e7b3bc1b60d31728c472065016d4f4e7
ImranRahimov1995 <imash.odessahouse@gmail.com>
2021-10-15 11:42:50 +0400

    Create model

--- project/chat/models.py
+++ project/chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='message_sended')
+    recipient = models.ForeignKey(User,on_delete=models.CASCADE,related_name='message_delivered')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    body = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.author.username} send message to {self.recipient.username}"
+
+    class Meta:
+        ordering = ('-created_at')
